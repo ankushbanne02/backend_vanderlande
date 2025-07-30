42b5df76ee9ecceb4e6991e0540531b311b2a2e1
shrutiphutane28 <shrutiphutane28@gmail.com>
2025-07-30 15:40:41 +0530

    THroughput backend added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import summary,volume  # Import your router module
+from app.routes import summary,volume, throughput  # Import your router module
 
 app = FastAPI(
     title="Parcel KPI API",
@@ -26,3 +26,4 @@ def root():
 # Register KPI summary route
 app.include_router(summary.router)
 app.include_router(volume.router)
+app.include_router(throughput.router)
