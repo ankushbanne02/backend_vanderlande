365909cc2db36e04500301aa0957bc36327ab6ff
Ankush Banne <banneankus456@gmail.com>
2025-07-30 12:14:45 +0530

    volume endpoint added

--- app/routes/volume.py
+++ app/routes/volume.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends, HTTPException
 from pymongo.database import Database
 from app.database.db import get_db
 from app.models.kpi_model import DateRequest
-from datetime import datetime
 from collections import defaultdict
 
 router = APIRouter()
@@ -22,29 +21,28 @@ def get_volume(payload: DateRequest, db: Database = Depends(get_db)):
             return {"message": "No data found for this date"}
 
         # Count occurrences
-        weight_count = defaultdict(int)
         height_count = defaultdict(int)
         width_count = defaultdict(int)
+        length_count = defaultdict(int)
 
         for parcel in parcels:
             volume = parcel.get("volume_data", {})
-
-            weight = volume.get("real_volume")
             height = volume.get("height")
             width = volume.get("width")
+            length = volume.get("length")
 
-            if weight is not None:
-                weight_count[weight] += 1
             if height is not None:
                 height_count[height] += 1
             if width is not None:
                 width_count[width] += 1
+            if length is not None:
+                length_count[length] += 1
 
         # Convert defaultdicts to dicts for JSON response
         return {
-            "weight_distribution": dict(weight_count),
             "height_distribution": dict(height_count),
-            "width_distribution": dict(width_count)
+            "width_distribution": dict(width_count),
+            "length_distribution": dict(length_count)
         }
 
     except Exception as e:
