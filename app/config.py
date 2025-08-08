dfa7079cf84a48de4fa4b7a46d4ba1a6644e4017
shrutiphutane28 <shrutiphutane28@gmail.com>
2025-08-08 12:03:27 +0530

    Final summary changes

--- app/config.py
+++ app/config.py
@@ -1,9 +1,11 @@
-import json
 import os
-
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.json")
+import json
 
 def load_config():
+    # Path to config.json inside the config folder
+    CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config", "config.json")
+
+    # Read and parse JSON
     with open(CONFIG_PATH, "r") as f:
         return json.load(f)
 
